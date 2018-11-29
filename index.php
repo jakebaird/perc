c7d9c290dccd8a91c11238fcf35f18caf631bf33
Joel Molloy <joel.m@kingkong.com.au>
2018-11-29 11:07:10 +1100

    Added Home Page Content

--- index.php
+++ index.php
@@ -137,7 +137,36 @@
                             <a id="show-more" href="#read-more">Read More</a> 
 <div id="read-more">
     <h1>Brisbane Tiling Services</h1>
-<p>Content coming soon.</p>
+<h2>Tiling Brisbane Residents Can Trust</h2>
+<p>Tiling is a powerful option for any project. It can come in a variety of materials and can work in many areas of the home to offer a durable, elegant and practical surface. Of course, tiling isn&rsquo;t just reserved for residential buildings, but it is also used extensively within commercial areas due to the modern and practical feel that it can create.</p>
+<p>Given that cleaning and general maintenance becomes easier, tiling is a long-lasting option which is easier to maintain and care for. This means that you can keep the brand-new look for years to come.</p>
+<p>Given these benefits, it&rsquo;s little wonder that tiling is such a popular choice. To get the best results, though, you&rsquo;ve got to find a <strong>Brisbane tiling service</strong> you can trust. If not, you run the risk of having a sub-standard job which leads to headaches, constant maintenance and in some extreme cases, structural damage.</p>
+<p>Countless businesses and residents trust PERC Tiling to bring their vision to life with the best materials and workmanship possible. Here&rsquo;s a deeper look at why we&rsquo;re the go-to <strong>tilers Brisbane City</strong> trusts.</p>
+<h2>Commercial and Residential Solutions</h2>
+<p>Many of our customers appreciate the breadth of our experience. Given that we are comfortable tackling both commercial and residential projects we offer the best of both worlds: large-scale capabilities with the delicate and careful touch that makes every project special.</p>
+<p>The two sides of our work influence the other to help us offer the best results regardless of your requirements or size. Our founder, Peter Russ, has 18 years of experience in offering both types of solutions and he has handpicked our team of highly-skilled industry professionals who are dedicated to getting the best results every single time.</p>
+<h2>Competitive Pricing</h2>
+<p>When it comes to <strong>tiling cost, Brisbane</strong> residents and businesses will find that we are truly unbeatable. Given our years of experience, we have developed the fairest pricing structures out there. This means that whatever the job we will suggest quotes which are attractive and clear. We are also happy to develop quotes according to your specific needs, rather than offering a one-size-fits-all approach to pricing.</p>
+<p>We believe that your project shouldn&rsquo;t break the bank and that you should be able to achieve fantastic results for a fair price. This underpins our pricing philosophy, and you&rsquo;ll find that it rings true in the quotes that we offer to you.&nbsp;</p>
+<h2>Award-Winning Partnerships</h2>
+<p>We are the trusted and capable partner of award-winning builders, architects and commercial developers. Given the results that we achieve and our comprehensively professional approach, we are trusted to get the best results on a range of luxury tiling projects. You can keep up with our projects by visiting our various social media accounts.</p>
+<p>If you&rsquo;re looking to <strong>find a tiler Brisbane</strong> businesses would point towards us, giving you confidence that we can achieve the best. In fact, you can find a list of some of our clients on our website.</p>
+<h2>Capturing Your Vision</h2>
+<p>Whatever your vision, we can capture it. Through close consultation, we will build up a pin-point accurate idea of what it is that you want and talk you through how we will achieve it. Whatever the service, you can rest assured we&rsquo;ll turn your job into a masterpiece. We frequently work with a range of materials including natural stone, travertine, granite, marble, mosaics and more.</p>
+<p>Our team of experts relish the opportunity to discuss a new project and bring it to life. You&rsquo;ll find that we are adept at developing projects from brief to execution, making it a breeze for you.</p>
+<h2>The Highest Standards</h2>
+<p>When it comes to looking for a <strong>good tiler, Brisbane</strong> residents have lots of options. We do sincerely believe, however, that our high standards set us apart from the crowd. Our attention to detail is unparalleled, and we will never leave a site without first having achieved the best possible results and confirming that everything is as good as it can possibly be.</p>
+<p>This is an important approach particularly with regards to tiling, where a lack of attention to detail can seriously derail the entire look of a particular finish.</p>
+<h2>Honest, Reliable and Committed</h2>
+<p>Aside from the quality of the work that we produce, we strive to offer courteous and friendly service which makes the entire process go much more pleasantly for everybody. We appreciate that our customers are under their own pressure which is why we aim to add value by making things as simple and straightforward as possible.</p>
+<p>We have worked on a countless number of projects and built strong ties with our customers. You will see from our reviews that our professional team is often praised for their customer-focused approach from the beginning of the process right until the very end.</p>
+<p>Above all else, we like to define our service as being honest, reliable and committed. We believe that this trio of words sums up what we are about and how we approach every single project.</p>
+<h2>A Free, No-Obligation Quote</h2>
+<p>We want you to be comfortable throughout the entire process of working with us, which is why we can provide you with a no-obligation quote based upon the details that you give to us. We are fast and attentive, aiming to get back to your enquiry within 24 hours on business day. You can contact us using your preferred method, whether that&rsquo;s via email, phone or our simple online form.</p>
+<h2>Learn More About Us Today</h2>
+<p>We understand that you&rsquo;re making a big decision, which is why we have made a concerted effort to make information about us available to you. You can visit our website to learn all that you need to know about us and even explore our social media channels to get closer to our team and the work that we do.</p>
+<p>We&rsquo;re also happy to answer any questions that you may have. Simply visit our website, read through our information and we look forward to hearing from you soon and creating a masterpiece for you.</p>
+<p>Perc Tiling are a professional <a href="/">Brisbane tiling service</a> specialising in <a href="/wall-tiles-brisbane.html">wall tiles</a>, <a href="/kitchen-tiles-brisbane.html">kitchen tiles</a> and general <a href="/tile-installation-brisbane.html">tile installation</a> services.</p> We also look after all <a href="/tile-repairs-brisbane.html">tile repairs</a> as well as all <a href="/residential-tiling-brisbane.html">residential </a>& <a href="/commercial-tiling-brisbane.html">commercial tiling</a> needs.</p>
 <a id="hide-less" href="#read-more">Read Less</a>
 </div>
                         </div>
